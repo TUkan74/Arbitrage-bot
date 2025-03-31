48a72d10ae5105ac0afa7e48086a4b2b76f9dcec
Matúš Klečka <klecka.matus@gmail.com>
2025-03-31 18:09:44 +0200

    arch(utils) Changed: moved around utils so it makes more sence

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,11 @@
-"""This module contains the utils for the project."""
+"""
+Utility modules for the arbitrage bot.
+"""
+
+from .logger import Logger
+from .timezone import convert_to_local_tz
+
+__all__ = [
+    'Logger',
+    'convert_to_local_tz'
+]
