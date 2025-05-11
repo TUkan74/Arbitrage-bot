a579e3f4148068bb6e01e23a900aae3ddf5d49aa
Matúš Klečka <klecka.matus@gmail.com>
2025-05-11 17:43:25 +0200

    debug(init)

--- src/core/exchanges/ccxt/__init__.py
+++ src/core/exchanges/ccxt/__init__.py
@@ -1,1 +1,11 @@
-"""CCXT integration for multi-exchange support."""
+"""
+CCXT wrapper for supporting multiple exchanges.
+"""
+
+from .ccxt_connector import CcxtExchange
+from .ccxt_normalizer import CcxtNormalizer
+
+__all__ = [
+    'CcxtExchange',
+    'CcxtNormalizer'
+]
